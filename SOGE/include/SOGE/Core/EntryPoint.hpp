01d4b11d9ef8311bb94a446e80e335aea279747a
Nikita Kozlov <Nyarstot@protonmail.com>
2024-10-28 22:17:31 +0300

    remove tests from entry point

--- SOGE/include/SOGE/Core/EntryPoint.hpp
+++ SOGE/include/SOGE/Core/EntryPoint.hpp
@@ -1,7 +1,6 @@
 #ifndef SOGE_CORE_ENTRYPOINT_HPP
 #define SOGE_CORE_ENTRYPOINT_HPP
 
-#include "SOGE/System/StringId.hpp"
 #include "SOGE/Utils/Logger.hpp"
 
 
@@ -20,33 +19,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
 #endif // SOGE_DEBUG
 
     soge::Logger::Init();
-
-    // TODO remove after testing
-    using namespace soge::string_id_literals;
-
-    auto logStringId = [](const soge::StringId& id) {
-        const auto cStr = id.GetString().data();
-        SOGE_INFO_LOG("String identifier with value \"{}\" has hash {} and pointer {}", cStr, id.GetHash(),
-                      static_cast<const void*>(cStr));
-    };
-
-    constexpr auto constId = "Hello, World"_sid;
-    logStringId(constId);
-
-    std::string string = "Hello, World";
-    const auto allocatedId = soge::StringId(string);
-    logStringId(allocatedId);
-    assert(allocatedId == constId && "Compile-time and runtime StringIds should be equal");
-
-    const auto reusedId = soge::StringId("Hello, World");
-    logStringId(reusedId);
-    assert(allocatedId.GetString().data() == allocatedId.GetString().data() &&
-           "StringId should be able to reuse existing strings at runtime");
-
-    string += "!";
-    const auto newId = soge::StringId(string);
-    logStringId(newId);
-    assert(allocatedId != newId && "StringId should not reuse existing strings if they have been modified");
 
     const auto app = soge::CreateApplication();
     app->Run();
--- SOGE/source/SOGE/System/StringId.cpp
+++ SOGE/source/SOGE/System/StringId.cpp
@@ -1,5 +1,4 @@
 #include "sogepch.hpp"
-
 #include "SOGE/System/StringId.hpp"
 
 
