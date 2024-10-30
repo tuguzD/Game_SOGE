13a6e79ad06ff36a2fca8c2180d100c8861fea36
tuguzT <timurka.tugushev@gmail.com>
2024-10-30 19:08:06 +0300

    Add event category for event type

---
+++ SOGE/include/SOGE/Core/EventCategory.hpp
@@ -0,0 +1,84 @@
+#ifndef SOGE_CORE_EVENTCATEGORY_HPP
+#define SOGE_CORE_EVENTCATEGORY_HPP
+
+#include <variant>
+
+
+namespace soge
+{
+    class EventCategory
+    {
+    public:
+        struct Default
+        {
+            constexpr auto operator<=>(const Default&) const noexcept = default;
+            constexpr bool operator==(const Default&) const noexcept = default;
+        };
+
+        struct Input
+        {
+            constexpr auto operator<=>(const Input&) const noexcept = default;
+            constexpr bool operator==(const Input&) const noexcept = default;
+        };
+
+        class Custom
+        {
+        private:
+            using Priority = std::uint32_t;
+            Priority m_priority;
+
+        public:
+            explicit constexpr Custom(Priority aPriority) noexcept;
+
+            explicit constexpr operator Priority() const noexcept;
+
+            constexpr auto operator<=>(const Custom&) const noexcept = default;
+            constexpr bool operator==(const Custom&) const noexcept = default;
+        };
+
+    private:
+        using Variants = std::variant<Default, Input, Custom>;
+        Variants m_variants;
+
+    public:
+        explicit constexpr EventCategory() noexcept = default;
+        constexpr EventCategory(const Variants& aVariants) noexcept;
+
+        template <typename T>
+        constexpr EventCategory(T&& aVariant) noexcept
+        requires std::constructible_from<Variants, T>;
+
+        [[nodiscard]]
+        constexpr bool IsCustom() const noexcept;
+
+        constexpr auto operator<=>(const EventCategory&) const noexcept = default;
+        constexpr bool operator==(const EventCategory&) const noexcept = default;
+    };
+
+    constexpr EventCategory::EventCategory(const Variants& aVariants) noexcept : m_variants(aVariants)
+    {
+    }
+
+    constexpr bool EventCategory::IsCustom() const noexcept
+    {
+        return std::holds_alternative<Custom>(m_variants);
+    }
+
+    template <typename T>
+    constexpr EventCategory::EventCategory(T&& aVariant) noexcept
+    requires std::constructible_from<Variants, T>
+        : m_variants(std::forward<T>(aVariant))
+    {
+    }
+
+    constexpr EventCategory::Custom::Custom(const Priority aPriority) noexcept : m_priority(aPriority)
+    {
+    }
+
+    constexpr EventCategory::Custom::operator Priority() const noexcept
+    {
+        return m_priority;
+    }
+}
+
+#endif // SOGE_CORE_EVENTCATEGORY_HPP
--- SOGE/include/SOGE/Core/EventType.hpp
+++ SOGE/include/SOGE/Core/EventType.hpp
@@ -1,30 +1,43 @@
 #ifndef SOGE_CORE_EVENTTYPE_HPP
 #define SOGE_CORE_EVENTTYPE_HPP
 
+#include "SOGE/Core/EventCategory.hpp"
 #include "SOGE/System/StringId.hpp"
 
 
 namespace soge
 {
-    class EventType final
+    class EventType
     {
     private:
         StringId m_id;
+        EventCategory m_category;
 
     public:
-        constexpr explicit EventType(const StringId& aId);
+        constexpr explicit EventType(const StringId& aId, EventCategory aCategory);
 
         [[nodiscard]]
         constexpr const StringId& GetId() const noexcept;
+        [[nodiscard]]
+        constexpr EventCategory GetCategory() const noexcept;
+
+        constexpr auto operator<=>(const EventType&) const noexcept = default;
+        constexpr bool operator==(const EventType&) const noexcept = default;
     };
 
-    constexpr EventType::EventType(const StringId& aId) : m_id(aId)
+    constexpr EventType::EventType(const StringId& aId, const EventCategory aCategory)
+        : m_id(aId), m_category(aCategory)
     {
     }
 
     constexpr const StringId& EventType::GetId() const noexcept
     {
         return m_id;
+    }
+
+    constexpr EventCategory EventType::GetCategory() const noexcept
+    {
+        return m_category;
     }
 }
 
