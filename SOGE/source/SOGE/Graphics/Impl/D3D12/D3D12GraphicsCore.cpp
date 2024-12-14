f278c4fb2c956f03bcac7e9632d54b182c05f1d9
tuguzT <timurka.tugushev@gmail.com>
2024-12-14 20:36:40 +0300

    Create NVRHI device (without graphics command queue for now)

--- SOGE/source/SOGE/Graphics/Impl/D3D12/D3D12GraphicsCore.cpp
+++ SOGE/source/SOGE/Graphics/Impl/D3D12/D3D12GraphicsCore.cpp
@@ -11,24 +11,26 @@ namespace soge
     {
         SOGE_INFO_LOG("Creating D3D12 render backend...");
 
+        SOGE_INFO_LOG("Choosing best rendering device...");
         nri::AdapterDesc bestAdapterDesc{};
         std::uint32_t adapterDescNum = 1;
-        SOGE_INFO_LOG("Choosing best rendering device...");
         NRIThrowIfFailed(nri::nriEnumerateAdapters(&bestAdapterDesc, adapterDescNum), "choosing best device");
         SOGE_INFO_LOG(R"(Rendering device "{}" was chosen...)", bestAdapterDesc.name);
 
+        SOGE_INFO_LOG("Creating render device...");
         nri::DeviceCreationDesc deviceCreationDesc{};
         deviceCreationDesc.graphicsAPI = nri::GraphicsAPI::D3D12;
         deviceCreationDesc.enableGraphicsAPIValidation = true;
         deviceCreationDesc.enableNRIValidation = true;
         deviceCreationDesc.adapterDesc = &bestAdapterDesc;
         deviceCreationDesc.allocationCallbacks = m_allocationCallbacks;
-        SOGE_INFO_LOG("Creating render device...");
         NRIThrowIfFailed(nri::nriCreateDevice(deviceCreationDesc, m_device), "creating render device");
 
         SOGE_INFO_LOG("Retrieving NRI interfaces...");
         const auto coreInterface = static_cast<nri::CoreInterface*>(&m_nriInterface);
         NRIThrowIfFailed(nri::nriGetInterface(*m_device, NRI_INTERFACE(nri::CoreInterface), coreInterface));
+        m_nriInterface.SetDeviceDebugName(*m_device, "SOGE D3D12 GPU device");
+
         const auto helperInterface = static_cast<nri::HelperInterface*>(&m_nriInterface);
         NRIThrowIfFailed(nri::nriGetInterface(*m_device, NRI_INTERFACE(nri::HelperInterface), helperInterface));
         const auto streamerInterface = static_cast<nri::StreamerInterface*>(&m_nriInterface);
@@ -39,22 +41,27 @@ namespace soge
         SOGE_INFO_LOG("Retrieving command queue...");
         NRIThrowIfFailed(m_nriInterface.GetCommandQueue(*m_device, nri::CommandQueueType::GRAPHICS, m_commandQueue),
                          "retrieving command queue");
+        m_nriInterface.SetCommandQueueDebugName(*m_commandQueue, "SOGE graphics command queue");
 
         SOGE_INFO_LOG("Creating frame fence...");
         NRIThrowIfFailed(m_nriInterface.CreateFence(*m_device, 0, m_frameFence), "creating frame fence");
+        m_nriInterface.SetFenceDebugName(*m_frameFence, "SOGE graphics frame fence");
 
-        // TODO: uncomment to get linker error
-        // const nvrhi::d3d12::DeviceDesc deviceDesc{
-        //     .errorCB = m_messageCallback.get(),
-        //     .pDevice = nri::nriGetInterface(),
-        //     .pGraphicsCommandQueue =
-        // };
-        // m_deviceWrapper = nvrhi::d3d12::createDevice(deviceDesc);
+        SOGE_INFO_LOG("Creating NVRHI device wrapper...");
+        const nvrhi::d3d12::DeviceDesc deviceDesc{
+            .errorCB = &m_messageCallback,
+            .pDevice = static_cast<ID3D12Device*>(m_nriInterface.GetDeviceNativeObject(*m_device)),
+            // .pGraphicsCommandQueue = static_cast<CommandQueueD3D12*>(m_commandQueue),
+        };
+        m_deviceWrapper = nvrhi::d3d12::createDevice(deviceDesc);
     }
 
     D3D12GraphicsCore::~D3D12GraphicsCore()
     {
         SOGE_INFO_LOG("Destroying D3D12 render backend...");
+
+        SOGE_INFO_LOG("Destroying NVRHI device wrapper...");
+        m_deviceWrapper = nullptr;
 
         if (m_commandQueue != nullptr)
         {
