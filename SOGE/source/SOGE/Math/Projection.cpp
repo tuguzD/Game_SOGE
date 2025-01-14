f3742ba595dda512107dbab25ac1cd99ae266b3c
tuguzT <timurka.tugushev@gmail.com>
2025-01-14 20:22:02 +0300

    Fix orthographic projection matrix

--- SOGE/source/SOGE/Math/Projection.cpp
+++ SOGE/source/SOGE/Math/Projection.cpp
@@ -12,8 +12,8 @@ namespace soge
     glm::mat4x4 PerspectiveProjection::ToMatrix(const float aWidth, const float aHeight, const float aNearPlane,
                                                 const float aFarPlane) const
     {
-        const float aspect = (aHeight != 0.0f) ? aWidth / aHeight : 0.0f;
-        return glm::perspectiveLH_ZO(m_fovY, aspect, aNearPlane, aFarPlane);
+        const float aspectRatio = (aHeight != 0.0f) ? aWidth / aHeight : 0.0f;
+        return glm::perspectiveLH_ZO(m_fovY, aspectRatio, aNearPlane, aFarPlane);
     }
 
     OrthographicProjection::OrthographicProjection(const glm::vec2 aUnits) : m_units{aUnits}
@@ -23,9 +23,8 @@ namespace soge
     glm::mat4x4 OrthographicProjection::ToMatrix(const float aWidth, const float aHeight, const float aNearPlane,
                                                  const float aFarPlane) const
     {
-        const float invAspect = (aWidth != 0.0f) ? aHeight / aWidth : 0.0f;
-        const float halfX = m_units.x / 2.0f;
-        const float halfY = m_units.y / 2.0f;
-        return glm::orthoLH_ZO(-halfX, halfX, halfY * invAspect, -halfY * invAspect, aNearPlane, aFarPlane);
+        const float inverseAspectRatio = (aWidth != 0.0f) ? aHeight / aWidth : 0.0f;
+        return glm::orthoLH_ZO(-m_units.x, m_units.x, -m_units.y * inverseAspectRatio, m_units.y * inverseAspectRatio,
+                               aNearPlane, aFarPlane);
     }
 }
