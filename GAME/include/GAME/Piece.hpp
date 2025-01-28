1fb4f64c1f6e81ce93c22878c4a1b42a88d7e4e5
Tugushev Damir Rustamovich <0damir.1tugushev@gmail.com>
2025-01-28 15:18:03 +0300

    Small tweaks

--- GAME/include/GAME/Piece.hpp
+++ GAME/include/GAME/Piece.hpp
@@ -1,12 +1,12 @@
 #ifndef GAME_PIECE_HPP
 #define GAME_PIECE_HPP
 
-#include "SOGE/Utils/UUID.hpp"
+#include <SOGE/Utils/UUID.hpp>
 
 
 namespace soge_game
 {
-    struct Piece final
+    class Piece final
     {
     public:
         UUIDv4::UUID uuid;
